977f6ef541131541c895708475e47cc64dcb6139
Mohammad Sheva <40586450+moshval@users.noreply.github.com>
2020-11-23 05:57:39 +0700

    Enemy/mob list
    Deskripsi masi kurang, sementara masih buat testing combat

---
+++ mobdata.pl
@@ -0,0 +1,14 @@
+/* Testing Purposes */
+
+/*mobdata(ID,Name,Type,MaxHealth,Level,Attack,Defense,SpecialAtk,Exp,Gold) */
+
+mobdata(-999,secret_boss,boss,99999,99,99999,99999,99999,99999,99999).
+
+/* EZ PZ (mob level 5 kebawah) */
+mobdata(1,slime,mob,100,1,50,5,75,100,100).
+mobdata(2,hilichurl,mob,200,2,80,50,100,200,150).
+mobdata(3,richgoblin,mob,500,1,50,10,0,200,3000).
+/* TBA */
+/* BOSSU Lv 10 for example */
+mobdata(50,doragon,boss,3000,10,800,300,950,1000,5000).
+mobdata(51,demonlord_paimon,boss,6666,66,666,666,1666,6666,6666).
