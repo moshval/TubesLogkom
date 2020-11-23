51297138e3c38533abbbc80c18d80e985e4225fb
raf555 <irsoekarno888@gmail.com>
2020-11-23 12:53:24 +0700

    fix dikit store

--- map.pl
+++ map.pl
@@ -21,22 +21,22 @@ printMap(X,Y) :- \+isLimitBawah(X,Y), \+isLimitAtas(X,Y), \+isLimitKiri(X,Y), \+
 
 w :- \+init(_), write('Game has not started yet!') ,!.
 w :- posX(X), posY(Y), Y2 is Y-1, (isLimitBawah(X,Y2); isLimitAtas(X,Y2); isLimitKiri(X,Y2); isLimitKanan(X,Y2)), write('There is wall, unable to move.'), !.
-w :- posX(X), posY(Y), Y2 is Y-1, isStore(X,Y2), write('Gunakan store. untuk membuka store.'), retract(posY(_)), asserta(posY(Y2)), !.
+w :- posX(X), posY(Y), Y2 is Y-1, isStore(X,Y2), write('Write `store.` to see store menu.'), retract(posY(_)), asserta(posY(Y2)), !.
 w :- posX(X), posY(Y), Y2 is Y-1, \+isStore(X,Y2), \+isLimitBawah(X,Y2), \+isLimitAtas(X,Y), \+isLimitKiri(X,Y), \+isLimitKanan(X,Y), retract(posY(_)), asserta(posY(Y2)), !.
 
 s :- \+init(_), write('Game has not started yet!') ,!.
 s :- posX(X), posY(Y), Y2 is Y+1, (isLimitBawah(X,Y2); isLimitAtas(X,Y2); isLimitKiri(X,Y2); isLimitKanan(X,Y2)), write('There is wall, unable to move.'), !.
-s :- posX(X), posY(Y), Y2 is Y+1, isStore(X,Y2), write('Gunakan store. untuk membuka store.'), retract(posY(_)), asserta(posY(Y2)), !.
+s :- posX(X), posY(Y), Y2 is Y+1, isStore(X,Y2), write('Write `store.` to see store menu.'), retract(posY(_)), asserta(posY(Y2)), !.
 s :- posX(X), posY(Y), Y2 is Y+1, \+isStore(X,Y2), \+isLimitBawah(X,Y2), \+isLimitAtas(X,Y), \+isLimitKiri(X,Y), \+isLimitKanan(X,Y), retract(posY(_)), asserta(posY(Y2)), !.
 
 a :- \+init(_), write('Game has not started yet!') ,!.
 a :- posX(X), posY(Y), X2 is X-1, (isLimitBawah(X2,Y); isLimitAtas(X2,Y); isLimitKiri(X2,Y); isLimitKanan(X2,Y)), write('There is wall, unable to move.'), !.
-a :- posX(X), posY(Y), X2 is X-1, isStore(X2,Y), write('Gunakan store. untuk membuka store.'), retract(posX(_)), asserta(posX(X2)), !.
+a :- posX(X), posY(Y), X2 is X-1, isStore(X2,Y), write('Write `store.` to see store menu.'), retract(posX(_)), asserta(posX(X2)), !.
 a :- posX(X), posY(Y), X2 is X-1, \+isStore(X2,Y), \+isLimitBawah(X2,Y), \+isLimitAtas(X,Y), \+isLimitKiri(X,Y), \+isLimitKanan(X,Y), retract(posX(_)), asserta(posX(X2)), !.
 
 d :- \+init(_), write('Game has not started yet!') ,!.
 d :- posX(X), posY(Y), X2 is X+1, (isLimitBawah(X2,Y); isLimitAtas(X2,Y); isLimitKiri(X2,Y); isLimitKanan(X2,Y)), write('There is wall, unable to move.'), !.
-d :- posX(X), posY(Y), X2 is X+1, isStore(X2,Y), write('Gunakan store. untuk membuka store.'), retract(posX(_)), asserta(posX(X2)), !.
+d :- posX(X), posY(Y), X2 is X+1, isStore(X2,Y), write('Write `store.` to see store menu.'), retract(posX(_)), asserta(posX(X2)), !.
 d :- posX(X), posY(Y), X2 is X+1, \+isStore(X2,Y), \+isLimitBawah(X2,Y), \+isLimitAtas(X,Y), \+isLimitKiri(X,Y), \+isLimitKanan(X,Y), retract(posX(_)), asserta(posX(X2)), !.
 
 map :- \+init(_), write('Game has not started yet!') ,!.
--- store.pl
+++ store.pl
@@ -5,33 +5,38 @@ store :- \+init(_), write('Game has not started yet'),!.
 store :- init(_), posX(X), posY(Y), \+isStore(X,Y), write('You are not in the store position!'),!.
 store :- init(_), posX(X), posY(Y), isStore(X,Y), write('Welcome to The Store \n What do you want to buy \n 1. Gacha (1000 gold)\n 2. Health Potion (100 gold)\n 3. I want to sell my items'), !.
 
-gacha :- player(Job, MaxHealth, Level, Health, Attack, Defense, Exp, Gold),
+gacha :- \+init(_), write('Game has not started yet'),!.
+gacha :- init(_), posX(X), posY(Y), \+isStore(X,Y), write('You are not in the store position!'),!.
+gacha :- player(Job, MaxHealth, Level, Attack, Defense, Exp, Gold),
+         NewGold is Gold - 1000,
+         NewGold < 0,
+         write('Insufficient amount of gold'),!.
+
+gacha :- player(Job, MaxHealth, Level, Attack, Defense, Exp, Gold),
          NewGold is Gold - 1000,
          NewGold > 0,
          random(1,3,Randomize),
          addItem(Randomize,Job,1),
-         asserta(player(Job, MaxHealth, Level, Health, Attack, Defense, Exp, NewGold)),
-         write("Congratulation, you've got ", item(Randomize, Name, Job, Type, Health, Attack, Defense)).
+         asserta(player(Job, MaxHealth, Level, Attack, Defense, Exp, NewGold)),
+         write("Congratulation, you've got ", item(Randomize, Name, Job, Type, Health, Attack, Defense)),!.
 
-gacha :- player(Job, MaxHealth, Level, Health, Attack, Defense, Exp, Gold),
-         NewGold is Gold - 1000,
-         NewGold < 0,
-         write('Insufficient amount of gold').
+potion :- \+init(_), write('Game has not started yet'),!.
+potion :- init(_), posX(X), posY(Y), \+isStore(X,Y), write('You are not in the store position!'),!.
+potion :- write('How many potion do you want to buy ?'),
+          read(BanyakPotion),
+          player(Job, MaxHealth, Level, Attack, Defense, Exp, Gold),
+          NewGold is Gold - BanyakPotion*100,
+          NewGold < 0,
+          write('Insufficient amount of gold'),!.
 
 potion :- write('How many potion do you want to buy ?'),
           read(BanyakPotion),
-          player(Job, MaxHealth, Level, Health, Attack, Defense, Exp, Gold),
+          player(Job, MaxHealth, Level, Attack, Defense, Exp, Gold),
           NewGold is Gold - BanyakPotion*100,
           NewGold > 0,
           addItem(99,Job,BanyakPotion),
-          asserta(player(Job, MaxHealth, Level, Health, Attack, Defense, Exp, NewGold)).
+          asserta(player(Job, MaxHealth, Level, Attack, Defense, Exp, NewGold)),!.
 
-potion :- write('How many potion do you want to buy ?'),
-          read(BanyakPotion),
-          player(Job, MaxHealth, Level, Health, Attack, Defense, Exp, Gold),
-          NewGold is Gold - BanyakPotion*100,
-          NewGold < 0,
-          write('Insufficient amount of gold').
 
 
 
