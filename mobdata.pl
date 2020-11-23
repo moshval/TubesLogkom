394f07c22e8934923fcc4431cd38e2d9376e1ecb
moshval <shevamohammad10@gmail.com>
2020-11-23 13:50:52 +0700

    update dikit

--- mobdata.pl
+++ mobdata.pl
@@ -2,13 +2,45 @@
 
 /*mobdata(ID,Name,Type,MaxHealth,Level,Attack,Defense,SpecialAtk,Exp,Gold) */
 
-mobdata(-999,secret_boss,boss,99999,99,99999,99999,99999,99999,99999).
-
-/* EZ PZ (mob level 5 kebawah) */
+/* EZ PZ (mob level 5 kebawah) / base level ; stat bakal dibuat scalable based on level, mungkin per 5 level(?) */
 mobdata(1,slime,mob,100,1,50,5,75,100,100).
-mobdata(2,hilichurl,mob,200,2,80,50,100,200,150).
+mobdata(2,hilichurl,mob,200,2,80,20,100,200,150).
 mobdata(3,richgoblin,mob,500,1,50,10,0,200,3000).
+mobdata(4,wolf,mob,250,3,90,30,120,200,200).
+mobdata(5,bandit,mob,400,5,100,40,110,210,250).
+mobdata(6,shaman,mob,300,5,120,30,150,250,200).
+mobdata(7,cultist,mob,400,5,120,40,200,250,200).
+mobdata(8,orc,mob,400,5,120,40,150,250,200).
+mobdata(9,boar,mob,100,1,60,10,150,150,150).
+mobdata(10,bonus_enemy,mob,5,1,0,0,0,0,0).
 /* TBA */
 /* BOSSU Lv 10 for example */
-mobdata(50,doragon,boss,3000,10,800,300,950,1000,5000).
-mobdata(51,demonlord_paimon,boss,6666,66,666,666,1666,6666,6666).
+mobdata(50,doragon,boss,4000,10,800,300,950,1000,5000).
+mobdata(51,kerberos,boss,2000,10,700,250,1200,1000,4000).
+mobdata(52,archmage,boss,2000,10,600,150,1500,1000,5000).
+mobdata(53,abyssknight,boss,3000,10,750,200,1300,1000,4500).
+
+mobdata(66,demonlord_paimon,boss,6666,66,666,666,1666,6666,6666).
+
+mobdata(-999,secret_boss,boss,99999,99,99999,99999,99999,99999,99999).
+
+/* Special name (if needed) */
+specialdata(slime,slime_bomb).
+specialdata(hilichurl,random_spin).
+specialdata(richgoblin,here_is_ur_gold).
+specialdata(wolf,auuuuuuuu).
+specialdata(bandit,stabstabstab).
+specialdata(shaman,curse_u).
+specialdata(cultist,awaken_my_master).
+specialdata(orc,orc_rant).
+specialdata(boar,sruduk).
+specialdata(bonus_enemy,oof).
+
+specialdata(doragon,fusrodah).
+specialdata(kerberos,use_our_heads).
+specialdata(archmage,adava_kedavra).
+specialdata(abyssknight,phantom_slash).
+specialdata(demonlord_paimon,how_about_we_explore_the_area_ahead_of_us_later).
+specialdata(secret_boss,getrekt).
+
+
