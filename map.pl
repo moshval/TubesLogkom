f95a45419598e477acbfc387a3bfa1c87bbf3dff
farhanyusuf <farhan.yusufakbar02@gmail.com>
2020-11-23 15:18:50 +0700

    show map after a,w,s,d

--- map.pl
+++ map.pl
@@ -20,24 +20,24 @@ printMap(X,Y) :- isStore(X,Y), write('S'), X1 is X+1, printMap(X1, Y).
 printMap(X,Y) :- \+isLimitBawah(X,Y), \+isLimitAtas(X,Y), \+isLimitKiri(X,Y), \+isLimitKanan(X,Y), write('-'), X2 is X+1, printMap(X2,Y).
 
 w :- \+init(_), write('Game has not started yet!') ,!.
-w :- posX(X), posY(Y), Y2 is Y-1, (isLimitBawah(X,Y2); isLimitAtas(X,Y2); isLimitKiri(X,Y2); isLimitKanan(X,Y2)), write('There is wall, unable to move.'), !.
-w :- posX(X), posY(Y), Y2 is Y-1, isStore(X,Y2), write('Write `store.` to see store menu.'), retract(posY(_)), asserta(posY(Y2)), !.
-w :- posX(X), posY(Y), Y2 is Y-1, \+isStore(X,Y2), \+isLimitBawah(X,Y2), \+isLimitAtas(X,Y), \+isLimitKiri(X,Y), \+isLimitKanan(X,Y), retract(posY(_)), asserta(posY(Y2)), !.
+w :- posX(X), posY(Y), Y2 is Y-1, (isLimitBawah(X,Y2); isLimitAtas(X,Y2); isLimitKiri(X,Y2); isLimitKanan(X,Y2)), write('There is wall, unable to move.'), map, !.
+w :- posX(X), posY(Y), Y2 is Y-1, isStore(X,Y2), write('Write `store.` to see store menu.\n'), retract(posY(_)), asserta(posY(Y2)), map, !.
+w :- posX(X), posY(Y), Y2 is Y-1, \+isStore(X,Y2), \+isLimitBawah(X,Y2), \+isLimitAtas(X,Y), \+isLimitKiri(X,Y), \+isLimitKanan(X,Y), retract(posY(_)), asserta(posY(Y2)), map, !.
 
 s :- \+init(_), write('Game has not started yet!') ,!.
-s :- posX(X), posY(Y), Y2 is Y+1, (isLimitBawah(X,Y2); isLimitAtas(X,Y2); isLimitKiri(X,Y2); isLimitKanan(X,Y2)), write('There is wall, unable to move.'), !.
-s :- posX(X), posY(Y), Y2 is Y+1, isStore(X,Y2), write('Write `store.` to see store menu.'), retract(posY(_)), asserta(posY(Y2)), !.
-s :- posX(X), posY(Y), Y2 is Y+1, \+isStore(X,Y2), \+isLimitBawah(X,Y2), \+isLimitAtas(X,Y), \+isLimitKiri(X,Y), \+isLimitKanan(X,Y), retract(posY(_)), asserta(posY(Y2)), !.
+s :- posX(X), posY(Y), Y2 is Y+1, (isLimitBawah(X,Y2); isLimitAtas(X,Y2); isLimitKiri(X,Y2); isLimitKanan(X,Y2)), write('There is wall, unable to move.'), map, !.
+s :- posX(X), posY(Y), Y2 is Y+1, isStore(X,Y2), write('Write `store.` to see store menu.\n'), retract(posY(_)), asserta(posY(Y2)), map,!.
+s :- posX(X), posY(Y), Y2 is Y+1, \+isStore(X,Y2), \+isLimitBawah(X,Y2), \+isLimitAtas(X,Y), \+isLimitKiri(X,Y), \+isLimitKanan(X,Y), retract(posY(_)), asserta(posY(Y2)), map, !.
 
 a :- \+init(_), write('Game has not started yet!') ,!.
-a :- posX(X), posY(Y), X2 is X-1, (isLimitBawah(X2,Y); isLimitAtas(X2,Y); isLimitKiri(X2,Y); isLimitKanan(X2,Y)), write('There is wall, unable to move.'), !.
-a :- posX(X), posY(Y), X2 is X-1, isStore(X2,Y), write('Write `store.` to see store menu.'), retract(posX(_)), asserta(posX(X2)), !.
-a :- posX(X), posY(Y), X2 is X-1, \+isStore(X2,Y), \+isLimitBawah(X2,Y), \+isLimitAtas(X,Y), \+isLimitKiri(X,Y), \+isLimitKanan(X,Y), retract(posX(_)), asserta(posX(X2)), !.
+a :- posX(X), posY(Y), X2 is X-1, (isLimitBawah(X2,Y); isLimitAtas(X2,Y); isLimitKiri(X2,Y); isLimitKanan(X2,Y)), write('There is wall, unable to move.'), map, !.
+a :- posX(X), posY(Y), X2 is X-1, isStore(X2,Y), write('Write `store.` to see store menu.\n'), retract(posX(_)), asserta(posX(X2)), map, !.
+a :- posX(X), posY(Y), X2 is X-1, \+isStore(X2,Y), \+isLimitBawah(X2,Y), \+isLimitAtas(X,Y), \+isLimitKiri(X,Y), \+isLimitKanan(X,Y), retract(posX(_)), asserta(posX(X2)), map, !.
 
 d :- \+init(_), write('Game has not started yet!') ,!.
-d :- posX(X), posY(Y), X2 is X+1, (isLimitBawah(X2,Y); isLimitAtas(X2,Y); isLimitKiri(X2,Y); isLimitKanan(X2,Y)), write('There is wall, unable to move.'), !.
-d :- posX(X), posY(Y), X2 is X+1, isStore(X2,Y), write('Write `store.` to see store menu.'), retract(posX(_)), asserta(posX(X2)), !.
-d :- posX(X), posY(Y), X2 is X+1, \+isStore(X2,Y), \+isLimitBawah(X2,Y), \+isLimitAtas(X,Y), \+isLimitKiri(X,Y), \+isLimitKanan(X,Y), retract(posX(_)), asserta(posX(X2)), !.
+d :- posX(X), posY(Y), X2 is X+1, (isLimitBawah(X2,Y); isLimitAtas(X2,Y); isLimitKiri(X2,Y); isLimitKanan(X2,Y)), write('There is wall, unable to move.'), map, !.
+d :- posX(X), posY(Y), X2 is X+1, isStore(X2,Y), write('Write `store.` to see store menu.\n'), retract(posX(_)), asserta(posX(X2)), map, !.
+d :- posX(X), posY(Y), X2 is X+1, \+isStore(X2,Y), \+isLimitBawah(X2,Y), \+isLimitAtas(X,Y), \+isLimitKiri(X,Y), \+isLimitKanan(X,Y), retract(posX(_)), asserta(posX(X2)), map, !.
 
 legend :-
 	write('Map Legends:'), nl,
