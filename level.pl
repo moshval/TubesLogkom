3360b0e28e363b9eec45ec7cf8fd906fb125138b
Alif Bhadrika Parikesit <alifbhadrika@gmail.com>
2020-11-24 13:16:16 +0700

    fix leveling

--- level.pl
+++ level.pl
@@ -1,15 +1,22 @@
 levelUp :-
     init(_),
+    player(_, _, _, _, _, _, _, Exp, _),
     maxExp(MaxExp),
-    player(Job, MaxHealth, Level, Health, Attack, Defense, Exp, Gold),
-    NewLevel is Level + 1,
-    NewMaxHealth is MaxHealth + 80,
+    Exp =< MaxExp,
+    !,fail.
+levelUp :-
+    init(_),
+    maxExp(MaxExp),
+    Exp >= MaxExp,
+    player(Job, MaxHealth, Level, Health, Attack, Defense, Special, Exp, Gold),
+    NewLevel is (Level + 1),
+    NewMaxHealth is (MaxHealth + 80),
     NewHealth is NewMaxHealth,
-    NewAttack is Attack + 20,
-    NewDefense is Defense + 20,
-    NewSpecial is Special + 20,
-    NewExp is Exp - MaxExp,
-    NewGold is Gold + 150,
+    NewAttack is (Attack + 20),
+    NewDefense is (Defense + 20),
+    NewSpecial is (Special + 20),
+    NewExp is (Exp - MaxExp),
+    NewGold is (Gold + 150),
     retract(player(Job, MaxHealth, Level, Health, Attack, Defense, Special, Exp, Gold)),
     asserta(player(Job, NewMaxHealth, NewLevel, NewHealth, NewAttack, NewDefense, NewSpecial, NewExp, NewGold)).
  
