0b8a4be4740e0ff2f35d4a13ae62d0a7072e1ba1
raf555 <irsoekarno888@gmail.com>
2020-11-24 09:08:57 +0700

    fix store

--- store.pl
+++ store.pl
@@ -8,13 +8,14 @@ store :- init(_), posX(X), posY(Y), isStore(X,Y), write('Welcome to The Store \n
 gacha :- \+init(_), write('Game has not started yet'),!.
 gacha :- init(_), posX(X), posY(Y), \+isStore(X,Y), write('You are not in the store position!'),!.
 
-gacha :- player(_,_,_,_,_,_,_,_,SGold),
+gacha :- player(_, _, _, _, _, _, _, _, SGold),
          item(_,_,SJob,_,_,_,_),
-         NewGold is SGold - 1000,
+         NewGold is SGold - 100,
          NewGold >= 0,
          random(1,3,Randomize),
          addItem(Randomize,SJob,1),
-         asserta(player(_,_,_,_,_,_,_,_,NewGold)),!.
+	 retract(player(Job, MaxHealth, Level, Health, Attack, Defense, Sepcial, Exp, _)),
+         asserta(player(Job, MaxHealth, Level, Health, Attack, Defense, Sepcial, Exp, NewGold)),!.
 
 gacha :- player(_,_,_,_,_,_,_,_,SGold),
          NewGold is SGold - 1000,
@@ -30,7 +31,8 @@ potion :- write('How many potion do you want to buy ? \n'),
           NewGold is SGold - BanyakPotion*100,
           NewGold >= 0,
           addItem(99,SJob,BanyakPotion),
-          asserta(player(_,_,_,_,_,_,_,_, NewGold)),!.
+	  retract(player(Job, MaxHealth, Level, Health, Attack, Defense, Sepcial, Exp, _)),
+          asserta(player(Job, MaxHealth, Level, Health, Attack, Defense, Sepcial, Exp, NewGold)),!.
 potion :- write('Insufficient amount of gold'),!.
 
 
